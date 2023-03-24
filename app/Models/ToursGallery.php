cf83c86803fe229f9faf98e4c223cf306d0e5225
adeirfanimanda <adeheaven09@gmail.com>
2023-03-24 16:07:37 +0700

    model foto tours

---
+++ app/Models/ToursGallery.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+
+class ToursGallery extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'tours_id',
+        'url',
+    ];
+
+    public function getUrlAttribute($url)
+    {
+        return config('app.url') . Storage::url($url);
+    }
+}
