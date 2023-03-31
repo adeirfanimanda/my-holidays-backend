a678ffd44aa3e008622dfb633e47c1ea685b6bc0
adeirfanimanda <adeheaven09@gmail.com>
2023-03-31 17:15:09 +0700

    crud kategori tours - formrequest

---
+++ app/Http/Controllers/ToursCategoryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ToursCategoryRequest;
+use App\Models\ToursCategory;
+use Illuminate\Http\Request;
+
+class ToursCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ToursCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ToursCategory  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ToursCategory $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ToursCategory  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ToursCategory $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ToursCategory  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ToursCategoryRequest $request, ToursCategory $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ToursCategory  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ToursCategory $category)
+    {
+        //
+    }
+}
